cbe437b411193f22a3f25e9d85aa55294fe534d5
Adrian Lopez <adrian2@caribe.net>
2013-05-27 18:30:57 -0400

    Added zlib license text.

--- dirchanges.c
+++ dirchanges.c
@@ -1,3 +1,23 @@
+/* DIRCHANGES Copyright (c) 2013 Adrian Lopez
+
+   This software is provided 'as-is', without any express or implied warranty.
+   In no event will the authors be held liable for any damages arising from the
+   use of this software.
+
+   Permission is granted to anyone to use this software for any purpose,
+   including commercial applications, and to alter it and redistribute it
+   freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+      claim that you wrote the original software. If you use this software in a
+      product, an acknowledgment in the product documentation would be 
+      appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+      misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source distribution.
+*/
 #include <archive.h>
 #include <archive_entry.h>
 #include <stdio.h>
@@ -873,7 +893,7 @@ int main(int argc, char **argv)
 		{ "froot", 1, 0, 'f' },
 		{ "troot", 1, 0, 't' },
 		{ "verbose", 0, 0, 'v' },
-		{ "printhashes", 0, 0, 'h' },
+		{ "hash", 0, 0, 'h' },
 		{ 0, 0, 0, 0 }
 	};
 
